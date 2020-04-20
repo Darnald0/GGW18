11f9d2445c57395d04b777955bceef8070930648
Romain "Nebrazif" Habib <romain0habib@gmail.com>
2020-04-20 16:49:03 +0200

    Adding Base Trigger Event Script
    Trigger Event Script using Rigidbody and isTrigger Box Collider

---
+++ GGW/Assets/EleveC/Scripts/EventTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LaunchEvent();
+        }
+    }
+
+    private void LaunchEvent()
+    {
+        Debug.Log("Launching Event");
+
+        // Lancer une animation
+    }
+
+}
