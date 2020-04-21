f363588fba79217cc75a30565492db137e197a50
Romain "Nebrazif" Habib <romain0habib@gmail.com>
2020-04-21 20:37:54 +0200

    Adding HoverShader with bool

--- GGW/Assets/EleveC/Scripts/SelectionFeedback.cs
+++ GGW/Assets/EleveC/Scripts/SelectionFeedback.cs
@@ -7,11 +7,13 @@ public class SelectionFeedback : MonoBehaviour
 {
     public bool isHovered;
 
+
     public Vector3 size;
 
     //[Header("Materials Prefs")]
     //public Material normalMaterial;
     //[SerializeField] private Material selectMaterial;
+    public Material hoverMat;
 
 
     private void Start()
@@ -30,13 +32,13 @@ public class SelectionFeedback : MonoBehaviour
         {
             //transform.GetComponent<Renderer>().material = selectMaterial;
             Debug.Log("HOVER");
-            GetComponent<Renderer>().material.SetFloat("_Outline", 0.02f);
+            hoverMat.SetFloat("Boolean_9D67F661", 1.0f);
         }
 
         else if (!isHovered)
         {
             //transform.GetComponent<Renderer>().material = normalMaterial;
-            GetComponent<Renderer>().material.SetFloat("_Outline", 0);
+            hoverMat.SetFloat("Boolean_9D67F661", 0.0f);
         }
     }
 }
