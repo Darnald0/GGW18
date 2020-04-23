fb62a959ba84f437d1274cc8b2f024413fbcc565
Darnald0 <55183284+Darnald0@users.noreply.github.com>
2020-04-23 16:12:13 +0200

    Add Change ingredient when player came back from lv1 or 2

---
+++ GGW/Assets/Final/Scripts/IngredientManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientManager : MonoBehaviour
+{
+    static public int ingredientAvailable = 1;
+    public GameObject ingredientA;
+    public GameObject ingredientB;
+    public GameObject ingredientC;
+
+    void Awake()
+    {
+        switch (ingredientAvailable)
+        {
+            case 1:
+                ingredientC.SetActive(false);
+                 break;
+            case 2:
+                ingredientB.SetActive(false);
+                break;
+            case 3:
+                ingredientA.SetActive(false);
+                break;
+            default:
+                Debug.Log("Placement ingredient error");
+                break;
+        }
+    }
+}
