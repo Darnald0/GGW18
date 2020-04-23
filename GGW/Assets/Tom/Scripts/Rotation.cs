fb5d10652f8ca093e947c0ecd594a6ef4e524597
Frizzbii <tom.grandjean.maglione@gmail.com>
2020-04-23 14:25:10 +0200

    boule disco

---
+++ GGW/Assets/Tom/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
