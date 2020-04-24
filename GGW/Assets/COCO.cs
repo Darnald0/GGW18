7ead387d8b891652d8d438ed8621f05d51855dad
Romain "Nebrazif" Habib <romain0habib@gmail.com>
2020-04-24 18:15:40 +0200

    Je record, si c'est bon c'est finito !

---
+++ GGW/Assets/COCO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class COCO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EndingScreen());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator EndingScreen()
+    {
+        yield return new WaitForSeconds(27);
+        SceneManager.LoadScene("Menu");
+    }
+}
